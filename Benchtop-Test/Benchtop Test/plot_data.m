89f6d36aa4688da0af0384d557c4120030c7abfe
ronangiss <46977169+ronangiss@users.noreply.github.com>
2023-01-19 11:54:08 -0500

    Updated Plots with Instantaneous Freq

--- Benchtop-Test/Benchtop Test/plot_data.m
+++ Benchtop-Test/Benchtop Test/plot_data.m
@@ -39,71 +39,71 @@ close all
 % ------------------------Plot All Data---------------------------
 % ----------------------------------------------------------------
 
-files = ["12_02_2022_benchtop_test/1Hz_25cycles_PDMSwing_experiment_120222.csv"
-         "12_02_2022_benchtop_test/1Hz_100cycles_CBwing_2000acc_experiment_120222.csv"
-         "12_02_2022_benchtop_test/2.5Hz_50cycles_PDMSwing_2000acc_experiment_120222.csv"
-         "12_02_2022_benchtop_test/2.5Hz_100cycles_CBwing_2000acc_experiment_120222.csv"
-         "12_02_2022_benchtop_test/2.5Hz_100cycles_PDMSwing_2000acc_experiment_120222.csv"
-         "12_02_2022_benchtop_test/2Hz_50cycles_PDMSwing_4000acc_experiment_120222.csv"
-         "12_02_2022_benchtop_test/2Hz_50cycles_PDMSwing_experiment_120222.csv"
-         "12_02_2022_benchtop_test/2Hz_100cycles_CBwing_2000acc_experiment_120222.csv"
-         "12_02_2022_benchtop_test/3Hz_100cycles_CBwing_2000acc_experiment_120222.csv"
-         "12_02_2022_benchtop_test/3Hz_100cycles_PDMSwing_2000acc_experiment_120222.csv"];
-
-for i = 1:length(files)
-    %%
-    
-    % Get case name from file name
-    case_name = erase(files(i), ["12_02_2022_benchtop_test/", "_experiment_120222.csv"]);
-    case_name = strrep(case_name,'_',' ');
-    
-    % Get data from file
-    data = readmatrix(files(i));
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-    
-    % Open a new figure.
-    f = figure;
-    f.Position = [200 50 900 560];
-
-    % Create three subplots to show the force time histories. 
-    subplot(2, 3, 1);
-    plot(times, force_vals(:, 1));
-    title("F_x");
-    xlabel("Time (s)");
-    ylabel("Force (N)");
-    subplot(2, 3, 2);
-    plot(times, force_vals(:, 2));
-    title("F_y");
-    xlabel("Time (s)");
-    ylabel("Force (N)");
-    subplot(2, 3, 3);
-    plot(times, force_vals(:, 3));
-    title("F_z");
-    xlabel("Time (s)");
-    ylabel("Force (N)");
-
-    % Create three subplots to show the moment time histories.
-    subplot(2, 3, 4);
-    plot(times, force_vals(:, 4));
-    title({"M_x" ""});
-    xlabel("Time (s)");
-    ylabel("Torque (N m)");
-    subplot(2, 3, 5);
-    plot(times, force_vals(:, 5));
-    title({"M_y" ""});
-    xlabel("Time (s)");
-    ylabel("Torque (N m)");
-    subplot(2, 3, 6);
-    plot(times, force_vals(:, 6));
-    title({"M_z" ""});
-    xlabel("Time (s)");
-    ylabel("Torque (N m)");
-    
-    % Label the whole figure.
-    sgtitle("Force Transducer Measurement for " + case_name);
-end
+% files = ["12_02_2022_benchtop_test/1Hz_25cycles_PDMSwing_experiment_120222.csv"
+%          "12_02_2022_benchtop_test/1Hz_100cycles_CBwing_2000acc_experiment_120222.csv"
+%          "12_02_2022_benchtop_test/2.5Hz_50cycles_PDMSwing_2000acc_experiment_120222.csv"
+%          "12_02_2022_benchtop_test/2.5Hz_100cycles_CBwing_2000acc_experiment_120222.csv"
+%          "12_02_2022_benchtop_test/2.5Hz_100cycles_PDMSwing_2000acc_experiment_120222.csv"
+%          "12_02_2022_benchtop_test/2Hz_50cycles_PDMSwing_4000acc_experiment_120222.csv"
+%          "12_02_2022_benchtop_test/2Hz_50cycles_PDMSwing_experiment_120222.csv"
+%          "12_02_2022_benchtop_test/2Hz_100cycles_CBwing_2000acc_experiment_120222.csv"
+%          "12_02_2022_benchtop_test/3Hz_100cycles_CBwing_2000acc_experiment_120222.csv"
+%          "12_02_2022_benchtop_test/3Hz_100cycles_PDMSwing_2000acc_experiment_120222.csv"];
+% 
+% for i = 1:length(files)
+%     %%
+%     
+%     % Get case name from file name
+%     case_name = erase(files(i), ["12_02_2022_benchtop_test/", "_experiment_120222.csv"]);
+%     case_name = strrep(case_name,'_',' ');
+%     
+%     % Get data from file
+%     data = readmatrix(files(i));
+% 
+%     times = data(1:end,1);
+%     force_vals = data(1:end,2:7);
+%     
+%     % Open a new figure.
+%     f = figure;
+%     f.Position = [200 50 900 560];
+% 
+%     % Create three subplots to show the force time histories. 
+%     subplot(2, 3, 1);
+%     plot(times, force_vals(:, 1));
+%     title("F_x");
+%     xlabel("Time (s)");
+%     ylabel("Force (N)");
+%     subplot(2, 3, 2);
+%     plot(times, force_vals(:, 2));
+%     title("F_y");
+%     xlabel("Time (s)");
+%     ylabel("Force (N)");
+%     subplot(2, 3, 3);
+%     plot(times, force_vals(:, 3));
+%     title("F_z");
+%     xlabel("Time (s)");
+%     ylabel("Force (N)");
+% 
+%     % Create three subplots to show the moment time histories.
+%     subplot(2, 3, 4);
+%     plot(times, force_vals(:, 4));
+%     title({"M_x" ""});
+%     xlabel("Time (s)");
+%     ylabel("Torque (N m)");
+%     subplot(2, 3, 5);
+%     plot(times, force_vals(:, 5));
+%     title({"M_y" ""});
+%     xlabel("Time (s)");
+%     ylabel("Torque (N m)");
+%     subplot(2, 3, 6);
+%     plot(times, force_vals(:, 6));
+%     title({"M_z" ""});
+%     xlabel("Time (s)");
+%     ylabel("Torque (N m)");
+%     
+%     % Label the whole figure.
+%     sgtitle("Force Transducer Measurement for " + case_name);
+% end
 
 %%
 
@@ -242,34 +242,47 @@ legend("Location","Southwest");
 
 % instantaneous frequency stuff I was trying...
 
-% % Open a new figure.
-% f = figure;
-% f.Position = [200 50 900 560];
-% title("Filtered Lift Force (z-direction)");
-% xlabel("Time (s)");
-% ylabel("Force (N)");
-% hold on
-% 
-% for i = 1:length(files)
-%     % Get case name from file name
-%     case_name = erase(files(i), ["12_02_2022_benchtop_test/", "_experiment_120222.csv"]);
-%     case_name = strrep(case_name,'_',' ');
-%     
-%     % Get data from file
-%     data = readmatrix(files(i));
-% 
-%     times = data(1:end,1);
-%     force_vals = data(1:end,2:7);
-%     
-%     % Filtering force transducer data with a butterworth filter
-%     fc = 3; % cutoff frequency
-%     fs = 1000; % sample frequency
-% 
-%     [b,a] = butter(6,fc/(fs/2)); % 6th order
-%     force_vals = filter(b, a, force_vals);
-% 
-% %     stft(force_vals(:, 3),fs,Window=kaiser(256,5),OverlapLength=220,FFTLength=512);)
-%     
-% %     instfreq(force_vals(:, 3),fs);
-% %     ylim([0,5]);
-% end
+% Open a new figure.
+f = figure;
+f.Position = [200 50 900 560];
+title("Instantaneous Frequency of Force in z-direction");
+xlabel("Time (s)");
+ylabel("Force Frequency i.e. Wing Speed (Hz)");
+hold on
+
+for i = 1:length(files)
+    % Get case name from file name
+    case_name = erase(files(i), ["12_02_2022_benchtop_test/", "_experiment_120222.csv"]);
+    case_name = strrep(case_name,'_',' ');
+    
+    % Get data from file
+    data = readmatrix(files(i));
+
+    times = data(1:end,1);
+    force_vals = data(1:end,2:7);
+    
+    % Filtering force transducer data with a butterworth filter
+    fc = 3; % cutoff frequency
+    fs = 1000; % sample frequency
+
+    [b,a] = butter(6,fc/(fs/2)); % 6th order
+    force_vals = filter(b, a, force_vals);
+
+%     [s,f,t] = stft(force_vals(:, 3),fs);
+%     stft(s(64,:),fs);
+    force_vals(:, 3) = force_vals(:, 3) - mean(force_vals(:, 3));
+%     stft(force_vals(:, 3),fs,'Window',kaiser(1024,5),'OverlapLength',500,'FFTLength',1024, 'FrequencyRange','centered');
+
+%     pspectrum(force_vals(:, 3),fs,'spectrogram');
+%     ylim([0,10]);
+
+    [ifq,t] = instfreq(force_vals(:, 3),fs);
+    
+    % Filtering instantaneous frequency with a butterworth filter
+    window = 100;
+    b = 1/window*ones(window,1);
+    ifq = filter(b, 1, ifq);
+    
+    plot(t, ifq, 'DisplayName', case_name, "LineWidth", 3)
+end
+legend("Location","Southwest");
