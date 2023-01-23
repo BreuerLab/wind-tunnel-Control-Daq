2ee1e6c328f7a73e461b25e5d54f261ef9ada5ae
ronangiss <46977169+ronangiss@users.noreply.github.com>
2023-01-23 12:19:15 -0500

    Added raw wingbeat normalized

--- Benchtop-Test/Benchtop Test/1_19_2022_benchtop_test/plot_data_1_19_2022.m
+++ Benchtop-Test/Benchtop Test/1_19_2022_benchtop_test/plot_data_1_19_2022.m
@@ -102,6 +102,9 @@ for i = 1:length(files)
     
     % Label the whole figure.
     sgtitle("Force Transducer Measurement for " + case_name);
+    
+    case_parts = char(split(case_name));
+    save([case_parts(2,:),'_',case_parts(1,1:end-1),'.mat'], 'data')
 end
 
 %%
@@ -109,18 +112,8 @@ end
 % ----------------------------------------------------------------
 % -------------------------Plot PDMS Data-------------------------
 % ----------------------------------------------------------------
-
-% files = ["1Hz_body_experiment_011923.csv"
-%          "2Hz_body_experiment_011923.csv"
-%          "3Hz_body_experiment_011923.csv"
-%          "4Hz_body_experiment_011923.csv"
-%          "5Hz_body_experiment_011923.csv"
-%          "6Hz_body_experiment_011923.csv"];
-
-files = ["1Hz_PDMS_experiment_011923.csv"
-         "2Hz_PDMS_experiment_011923.csv"
-         "3Hz_PDMS_experiment_011923.csv"
-         "4Hz_PDMS_experiment_011923.csv"];
+     
+cases = ["PDMS_1Hz", "PDMS_2Hz", "PDMS_3Hz", "PDMS_4Hz"];
 
 % Open a new figure.
 f = figure;
@@ -130,21 +123,17 @@ xlabel("Time (s)");
 ylabel("Force (N)");
 hold on
 
-for i = 1:length(files) 
-    % Get case name from file name
-    case_name = erase(files(i), "_experiment_011923.csv");
-    case_name = strrep(case_name,'_',' ');
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name);
     
-    % Get data from file
-    data = readmatrix(files(i));
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
+    case_name = strrep(cases(i),'_',' ');
 
     % Trimming off end of data (it appears beginning is already
-    % trimmed)
+    % trimmed) when differences between measurements are small
     count = 0;
-    vertical_diffs = diff(force_vals(:,3));
+    vertical_diffs = diff(data(:,4));
     for j = 1:length(vertical_diffs)
         if (abs(vertical_diffs(j)) < 0.05)
             count = count + 1;
@@ -156,6 +145,9 @@ for i = 1:length(files)
             break
         end
     end
+    
+    % resave data after trimming it
+    save(cases(i) + ".mat", 'data')
 
     times = data(1:end,1);
     force_vals = data(1:end,2:7);
@@ -166,32 +158,12 @@ end
 legend("Location","Southwest");
 ax1 = axes('Position',[0.35 0.2 0.2 0.2]);
 hold on
-for i = 1:length(files)
-    % Get case name from file name
-    case_name = erase(files(i), "_experiment_011923.csv");
-    case_name = strrep(case_name,'_',' ');
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name);
     
-    % Get data from file
-    data = readmatrix(files(i));
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-
-    % Trimming off end of data (it appears beginning is already
-    % trimmed)
-    count = 0;
-    vertical_diffs = diff(force_vals(:,3));
-    for j = 1:length(vertical_diffs)
-        if (abs(vertical_diffs(j)) < 0.05)
-            count = count + 1;
-        else
-            count = 0;
-        end
-        if (count > 5)
-            data = data(1:j-1000, :);
-            break
-        end
-    end
+    case_name = strrep(cases(i),'_',' ');
 
     times = data(1:end,1);
     force_vals = data(1:end,2:7);
@@ -215,13 +187,9 @@ annotation('arrow',[0.45 0.39], [0.4 0.52])
 % ----------------------------------------------------------------
 % ----------------------Plot Wingless Data------------------------
 % ----------------------------------------------------------------
-
-files = ["1Hz_body_experiment_011923.csv"
-         "2Hz_body_experiment_011923.csv"
-         "3Hz_body_experiment_011923.csv"
-         "4Hz_body_experiment_011923.csv"
-         "5Hz_body_experiment_011923.csv"
-         "6Hz_body_experiment_011923.csv"];
+     
+cases = ["Body_1Hz", "Body_2Hz", "Body_3Hz", ...
+         "Body_4Hz", "Body_5Hz", "Body_6Hz"];
 
 % Open a new figure.
 f = figure;
@@ -231,21 +199,17 @@ xlabel("Time (s)");
 ylabel("Force (N)");
 hold on
 
-for i = 1:length(files)
-    % Get case name from file name
-    case_name = erase(files(i), "_experiment_011923.csv");
-    case_name = strrep(case_name,'_',' ');
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name);
     
-    % Get data from file
-    data = readmatrix(files(i));
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
+    case_name = strrep(cases(i),'_',' ');
 
     % Trimming off end of data (it appears beginning is already
     % trimmed)
     count = 0;
-    vertical_diffs = diff(force_vals(:,3));
+    vertical_diffs = diff(data(:,4));
     for j = 1:length(vertical_diffs)
         if (abs(vertical_diffs(j)) < 0.05)
             count = count + 1;
@@ -258,6 +222,9 @@ for i = 1:length(files)
         end
     end
 
+    % resave data after trimming it
+    save(cases(i) + ".mat", 'data')
+    
     times = data(1:end,1);
     force_vals = data(1:end,2:7);
     
@@ -271,10 +238,8 @@ legend("Location","Southwest");
 % ----------------------------------------------------------------
 % -------Plot PDMS and Wingless Data at 1 Hz, 2 Hz, and 3 Hz------
 % ----------------------------------------------------------------
-
-files = ["1Hz_PDMS_experiment_011923.csv"
-         "2Hz_PDMS_experiment_011923.csv"
-         "3Hz_PDMS_experiment_011923.csv"];
+     
+cases = ["PDMS_1Hz", "PDMS_2Hz", "PDMS_3Hz"];
 
 % Open a new figure.
 f = figure;
@@ -284,32 +249,12 @@ title("PDMS Wings");
 xlabel("Time (s)");
 ylabel("Force (N)");
 hold on
-for i = 1:length(files)
-    % Get case name from file name
-    case_name = erase(files(i), "_experiment_011923.csv");
-    case_name = strrep(case_name,'_',' ');
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name);
     
-    % Get data from file
-    data = readmatrix(files(i));
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-
-    % Trimming off end of data (it appears beginning is already
-    % trimmed)
-    count = 0;
-    vertical_diffs = diff(force_vals(:,3));
-    for j = 1:length(vertical_diffs)
-        if (abs(vertical_diffs(j)) < 0.05)
-            count = count + 1;
-        else
-            count = 0;
-        end
-        if (count > 5)
-            data = data(1:j-1000, :);
-            break
-        end
-    end
+    case_name = strrep(cases(i),'_',' ');
 
     times = data(1:end,1);
     force_vals = data(1:end,2:7);
@@ -319,41 +264,20 @@ for i = 1:length(files)
 end
 legend("Location","Southwest");
 
-files = ["1Hz_body_experiment_011923.csv"
-         "2Hz_body_experiment_011923.csv"
-         "3Hz_body_experiment_011923.csv"];
+%-------------------------Body Only Plot-----------------------------
+cases = ["Body_1Hz", "Body_2Hz", "Body_3Hz"];
 
 subplot(1,2,2)
 title("Body Only");
 xlabel("Time (s)");
 ylabel("Force (N)");
 hold on
-for i = 1:length(files)
-    % Get case name from file name
-    case_name = erase(files(i), "_experiment_011923.csv");
-    case_name = strrep(case_name,'_',' ');
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name);
     
-    % Get data from file
-    data = readmatrix(files(i));
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-
-    % Trimming off end of data (it appears beginning is already
-    % trimmed)
-    count = 0;
-    vertical_diffs = diff(force_vals(:,3));
-    for j = 1:length(vertical_diffs)
-        if (abs(vertical_diffs(j)) < 0.05)
-            count = count + 1;
-        else
-            count = 0;
-        end
-        if (count > 5)
-            data = data(1:j-1000, :);
-            break
-        end
-    end
+    case_name = strrep(cases(i),'_',' ');
 
     times = data(1:end,1);
     force_vals = data(1:end,2:7);
@@ -370,13 +294,8 @@ sgtitle("Lift Force (z-direction)");
 % -------Plot PDMS and Wingless Data at 1 Hz, 2 Hz, and 3 Hz------
 % -------------------normalized by wing cycles--------------------
 % ----------------------------------------------------------------
-
-files = ["1Hz_PDMS_experiment_011923.csv"
-         "2Hz_PDMS_experiment_011923.csv"
-         "3Hz_PDMS_experiment_011923.csv"];
-
-% flipped so that largest amplitude goes in back of plot
-files = flip(files);
+     
+cases = ["PDMS_1Hz", "PDMS_3Hz", "PDMS_2Hz"];
 
 % Open a new figure.
 f = figure;
@@ -386,36 +305,15 @@ title("PDMS Wings");
 xlabel("Wingbeat Number");
 ylabel("Force (N)");
 hold on
-for i = 1:length(files)
-    % Get case name from file name
-    case_name = erase(files(i), "_experiment_011923.csv");
-    case_name = strrep(case_name,'_',' ');
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name);
     
-    % Get data from file
-    data = readmatrix(files(i));
+    case_name = strrep(cases(i),'_',' ');
 
     times = data(1:end,1);
     force_vals = data(1:end,2:7);
-
-    % Trimming off end of data (it appears beginning is already
-    % trimmed)
-    count = 0;
-    vertical_diffs = diff(force_vals(:,3));
-    for j = 1:length(vertical_diffs)
-        if (abs(vertical_diffs(j)) < 0.05)
-            count = count + 1;
-        else
-            count = 0;
-        end
-        if (count > 5)
-            data = data(1:j-1000, :);
-            break
-        end
-    end
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-    force_SDs = round(std(force_vals), 3);
 
     % Filtering force data with moving average filter
     window = 100;
@@ -424,8 +322,8 @@ for i = 1:length(files)
     
     % Count the number of wingbeats
     wingbeat_count = 0;
-    case_name = convertStringsToChars(case_name);
-    speed = str2double(case_name(1));
+    case_name = char(case_name);
+    speed = str2double(case_name(6));
     window = round(400 / speed);
     start_index = 0;
     end_index = 0;
@@ -439,49 +337,55 @@ for i = 1:length(files)
             end
         end
     end
-    disp(wingbeat_count)
     
+    data = data(start_index:end_index,:);
     filtered_lift_vals = filtered_lift_vals(start_index:end_index);
     wingbeats = linspace(0,wingbeat_count,length(filtered_lift_vals));
 
+    disp(wingbeat_count)
     disp(length(filtered_lift_vals))
+    
+    force_vals = data(1:end,2:7);
     % Plot lift force
-    plot(wingbeats, filtered_lift_vals, 'DisplayName', case_name, "LineWidth",3);
-    save(['Lift_PDMS_',num2str(speed),'Hz.mat'], 'filtered_lift_vals','wingbeats')
+    plot(wingbeats, force_vals(:,3), 'DisplayName', case_name, "LineWidth",3);
+    save(cases(i) + ".mat", 'data', 'filtered_lift_vals','wingbeats');
 end
 legend("Location","Southwest");
 ax1 = axes('Position',[0.15 0.26 0.2 0.2]);
 hold on
-for i = 1:length(files)
-    % Get case name from file name
-    case_name = erase(files(i), "_experiment_011923.csv");
-    case_name = strrep(case_name,'_',' ');
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name,'data','wingbeats');
     
-    % Get data from file
-    data = readmatrix(files(i));
+    force_vals = data(1:end,2:7);
+    
+    % Plot lift force
+    plot(ax1, wingbeats, force_vals(:,3));
+end
+xlim([0, 3])
+ylim([-3, 3])
+y_axis = line(xlim, [0 0], 'Color','black');
+box on
+annotation('arrow',[0.25 0.14], [0.46 0.52])
+
+%-------------------------Body Only Plot-----------------------------
+cases = ["Body_1Hz", "Body_3Hz", "Body_2Hz"];
+
+subplot(1,2,2)
+title("Body Only");
+xlabel("Wingbeat Number");
+ylabel("Force (N)");
+hold on
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name);
+    
+    case_name = strrep(cases(i),'_',' ');
 
     times = data(1:end,1);
     force_vals = data(1:end,2:7);
-
-    % Trimming off end of data (it appears beginning is already
-    % trimmed)
-    count = 0;
-    vertical_diffs = diff(force_vals(:,3));
-    for j = 1:length(vertical_diffs)
-        if (abs(vertical_diffs(j)) < 0.05)
-            count = count + 1;
-        else
-            count = 0;
-        end
-        if (count > 5)
-            data = data(1:j-1000, :);
-            break
-        end
-    end
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-    force_SDs = round(std(force_vals), 3);
 
     % Filtering force data with moving average filter
     window = 100;
@@ -490,8 +394,8 @@ for i = 1:length(files)
     
     % Count the number of wingbeats
     wingbeat_count = 0;
-    case_name = convertStringsToChars(case_name);
-    speed = str2double(case_name(1));
+    case_name = char(case_name);
+    speed = str2double(case_name(6));
     window = round(400 / speed);
     start_index = 0;
     end_index = 0;
@@ -506,9 +410,74 @@ for i = 1:length(files)
         end
     end
     
+    data = data(start_index:end_index,:);
     filtered_lift_vals = filtered_lift_vals(start_index:end_index);
     wingbeats = linspace(0,wingbeat_count,length(filtered_lift_vals));
 
+    disp(wingbeat_count)
+    disp(length(filtered_lift_vals))
+    
+    force_vals = data(1:end,2:7);
+    % Plot lift force
+    plot(wingbeats, force_vals(:,3), 'DisplayName', case_name, "LineWidth",3);
+    save(cases(i) + ".mat", 'data', 'filtered_lift_vals','wingbeats');
+end
+legend("Location","Southwest");
+ax2 = axes('Position',[0.6 0.26 0.2 0.2]);
+hold on
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name,'data','wingbeats');
+
+    force_vals = data(1:end,2:7);
+    
+    % Plot lift force
+    plot(ax2, wingbeats, force_vals(:,3));
+end
+xlim([0, 3])
+ylim([-3, 3])
+y_axis = line(xlim, [0 0], 'Color','black');
+box on
+annotation('arrow',[0.7 0.58], [0.46 0.48])
+
+sgtitle("Lift Force (z-direction)");
+
+%%
+
+% ----------------------------------------------------------------
+% -------Plot PDMS and Wingless Data at 1 Hz, 2 Hz, and 3 Hz------
+% -------------normalized by wing cycles and filtered-------------
+% ----------------------------------------------------------------
+     
+cases = ["PDMS_3Hz", "PDMS_2Hz", "PDMS_1Hz"];
+
+% Open a new figure.
+f = figure;
+f.Position = [200 50 900 560];
+subplot(1,2,1)
+title("PDMS Wings");
+xlabel("Wingbeat Number");
+ylabel("Force (N)");
+hold on
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name);
+    
+    case_name = strrep(cases(i),'_',' ');
+
+    % Plot lift force
+    plot(wingbeats, filtered_lift_vals, 'DisplayName', case_name, "LineWidth",3);
+end
+legend("Location","Southwest");
+ax1 = axes('Position',[0.15 0.26 0.2 0.2]);
+hold on
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name,'filtered_lift_vals','wingbeats');
+    
     % Plot lift force
     plot(ax1, wingbeats, filtered_lift_vals);
 end
@@ -518,139 +487,31 @@ y_axis = line(xlim, [0 0], 'Color','black');
 box on
 annotation('arrow',[0.25 0.14], [0.46 0.52])
 
-files = ["1Hz_body_experiment_011923.csv"
-         "2Hz_body_experiment_011923.csv"
-         "3Hz_body_experiment_011923.csv"];
-
-% flipped so that largest amplitude goes in back of plot
-files = flip(files);
+%-------------------------Body Only Plot-----------------------------
+cases = ["Body_3Hz", "Body_2Hz", "Body_1Hz"];
 
 subplot(1,2,2)
 title("Body Only");
 xlabel("Wingbeat Number");
 ylabel("Force (N)");
 hold on
-for i = 1:length(files)
-    % Get case name from file name
-    case_name = erase(files(i), "_experiment_011923.csv");
-    case_name = strrep(case_name,'_',' ');
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name);
     
-    % Get data from file
-    data = readmatrix(files(i));
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-
-    % Trimming off end of data (it appears beginning is already
-    % trimmed)
-    count = 0;
-    vertical_diffs = diff(force_vals(:,3));
-    for j = 1:length(vertical_diffs)
-        if (abs(vertical_diffs(j)) < 0.05)
-            count = count + 1;
-        else
-            count = 0;
-        end
-        if (count > 5)
-            data = data(1:j-1000, :);
-            break
-        end
-    end
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-
-    % Filtering force data with moving average filter
-    window = 100;
-    b = 1/window*ones(window,1);
-    filtered_lift_vals = filter(b, 1, force_vals(:, 3));
+    case_name = strrep(cases(i),'_',' ');
     
-    % Count the number of wingbeats
-    wingbeat_count = 0;
-    case_name = convertStringsToChars(case_name);
-    speed = str2double(case_name(1));
-    window = round(400 / speed);
-    start_index = 0;
-    end_index = 0;
-    for j = (1 + window):(length(filtered_lift_vals) - window)
-        if (filtered_lift_vals(j) == max(filtered_lift_vals(j-window:j+window)))
-            wingbeat_count = wingbeat_count + 1;
-            if (start_index == 0)
-                start_index = j;
-            else
-                end_index = j;
-            end
-        end
-    end
-    disp(wingbeat_count)
-    
-    filtered_lift_vals = filtered_lift_vals(start_index:end_index);
-    wingbeats = linspace(0,wingbeat_count,length(filtered_lift_vals));
-
-    disp(length(filtered_lift_vals))
     % Plot lift force
     plot(wingbeats, filtered_lift_vals, 'DisplayName', case_name, "LineWidth",3);
-    save(['Lift_body_',num2str(speed),'Hz.mat'], 'filtered_lift_vals','wingbeats')
 end
 legend("Location","Southwest");
 ax2 = axes('Position',[0.6 0.26 0.2 0.2]);
 hold on
-for i = 1:length(files)
-    % Get case name from file name
-    case_name = erase(files(i), "_experiment_011923.csv");
-    case_name = strrep(case_name,'_',' ');
-    
-    % Get data from file
-    data = readmatrix(files(i));
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-
-    % Trimming off end of data (it appears beginning is already
-    % trimmed)
-    count = 0;
-    vertical_diffs = diff(force_vals(:,3));
-    for j = 1:length(vertical_diffs)
-        if (abs(vertical_diffs(j)) < 0.05)
-            count = count + 1;
-        else
-            count = 0;
-        end
-        if (count > 5)
-            data = data(1:j-1000, :);
-            break
-        end
-    end
-
-    times = data(1:end,1);
-    force_vals = data(1:end,2:7);
-    force_SDs = round(std(force_vals), 3);
-
-    % Filtering force data with moving average filter
-    window = 100;
-    b = 1/window*ones(window,1);
-    filtered_lift_vals = filter(b, 1, force_vals(:, 3));
-    
-    % Count the number of wingbeats
-    wingbeat_count = 0;
-    case_name = convertStringsToChars(case_name);
-    speed = str2double(case_name(1));
-    window = round(400 / speed);
-    start_index = 0;
-    end_index = 0;
-    for j = (1 + window):(length(filtered_lift_vals) - window)
-        if (filtered_lift_vals(j) == max(filtered_lift_vals(j-window:j+window)))
-            wingbeat_count = wingbeat_count + 1;
-            if (start_index == 0)
-                start_index = j;
-            else
-                end_index = j;
-            end
-        end
-    end
-    
-    filtered_lift_vals = filtered_lift_vals(start_index:end_index);
-    wingbeats = linspace(0,wingbeat_count,length(filtered_lift_vals));
+for i = 1:length(cases)
+    % Load data
+    mat_name = cases(i) + ".mat";
+    load(mat_name,'filtered_lift_vals','wingbeats');
 
     % Plot lift force
     plot(ax2, wingbeats, filtered_lift_vals);
