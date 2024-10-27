06a07cea8c1ae25f8f6ab6d377e13a6d3c653007
ronangiss <46977169+ronangiss@users.noreply.github.com>
2024-10-27 10:11:06 -0400

    hiding slack

--- Flapperoo/Wind Tunnel Test/data processing/general/posToChord.m
+++
@@ -1,6 +0,0 @@
-function [pos_LE, pos_chord] = posToChord(pos)
-    [center_to_LE, chord] = getWingMeasurements();
-    pos_LE = pos + center_to_LE;
-    pos_chord = (pos_LE / chord) * 100;
-end
-
--- Flapperoo/Wind Tunnel Test/data processing/robot_parameters/get_sampling_info.m
+++
@@ -1,6 +0,0 @@
-function [frame_rate, num_wingbeats] = get_sampling_info()
-
-    frame_rate = 9000; % DAQ data sampling rate (Hz)
-    num_wingbeats = 180; % Number of wingbeats recorded for each trial
-
-end
