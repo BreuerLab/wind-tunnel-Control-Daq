eb8ab9935e4e6e371e8144100a6bc6ce3783e030
ronangiss <46977169+ronangiss@users.noreply.github.com>
2025-11-12 15:41:24 -0500

    Update estimate_duration.m

--- Calimero/Wind Tunnel Test/experiment functions/estimate_duration.m
+++ Calimero/Wind Tunnel Test/experiment functions/estimate_duration.m
@@ -5,7 +5,7 @@
 % Ronan Gissler
 % July 2025
 
-function [num_revs, session_duration, time_to_speed, at_speed_pos] = estimate_duration(vel, acc, measure_revs, padding_revs, hold_time, print_bool)
+function [num_revs, session_duration, time_to_speed, at_speed_pos] = estimate_duration(vel, acc, measure_revs, padding_revs, hold_time, wait_time, print_bool)
 
     time_to_speed = vel / acc;
     if (print_bool)
@@ -26,7 +26,7 @@ function [num_revs, session_duration, time_to_speed, at_speed_pos] = estimate_du
         session_duration = hold_time;
     else
         num_revs = measure_revs + 2*(padding_revs + round((at_speed_pos) + 0.5));
-        session_duration = round((num_revs / vel) + 2*time_to_speed); % 2*(wait_time/1000)
+        session_duration = round((num_revs / vel) + 2*time_to_speed) + 2*(wait_time/1000);
     end
     if (print_bool)
     disp(num_revs ...
