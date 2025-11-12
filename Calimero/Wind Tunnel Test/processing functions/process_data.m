d2eca88e5acd5ecefec18d7273cd8ae3e0cfc578
ronangiss <46977169+ronangiss@users.noreply.github.com>
2025-11-12 15:43:56 -0500

    Update process_data.m

--- Calimero/Wind Tunnel Test/processing functions/process_data.m
+++ Calimero/Wind Tunnel Test/processing functions/process_data.m
@@ -1,7 +1,17 @@
-function [time, force, voltAdj, curAdj, enc_pulse] = process_data(results, offsets, cal_matrix)
+function [time, force, voltAdj, curAdj, enc_pulse, filtered_speed] = process_data(results, offsets, cal_matrix)
     time = results(:,1);
     force = volt_to_force(results(:,2:7), offsets, cal_matrix);
     voltAdj = voltM_to_voltA(results(:,8), offsets(1,7));
     curAdj = volt_to_cur(results(:,9), offsets(1,8));
     enc_pulse = results(:,10);
+
+    dt = time(2) - time(1);
+    speed = gradient(results(:,11), dt);
+    ticksPerRev = 18432;
+    ticksPerPulse = 4;
+    speed = speed / (ticksPerRev / ticksPerPulse);
+    fc = 200;
+    fs = round(1 / dt);
+    [b,a] = butter(6,fc/(fs/2));
+    filtered_speed = filtfilt(b,a,speed);
 end
