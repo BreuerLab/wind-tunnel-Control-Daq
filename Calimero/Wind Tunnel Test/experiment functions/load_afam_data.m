573f7a9f72c97f72accaa8d32c7c55085aed35ed
zjrosoff <zjrosoff@gmail.com>
2026-01-29 18:14:29 -0500

    load_afam_data changed

--- Calimero/Wind Tunnel Test/experiment functions/load_afam_data.m
+++ Calimero/Wind Tunnel Test/experiment functions/load_afam_data.m
@@ -1,7 +1,8 @@
 function AFAM_Tunnel = load_afam_data
 
     try
-        load("R:\ENG_Breuer_Shared\group\AFAM_state.mat");
+        data = load("R:\ENG_Breuer_Shared\group\AFAM_state.mat");
+        AFAM_Tunnel = data.AFAM_Tunnel;   
     catch
         AFAM_Tunnel = load_afam_data;
     end
