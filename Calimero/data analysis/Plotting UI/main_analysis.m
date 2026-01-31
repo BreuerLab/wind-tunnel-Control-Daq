426df8089a21df54eb1a4bad0b55f9782d7b6c57
ronangiss <46977169+ronangiss@users.noreply.github.com>
2026-01-31 12:01:56 -0500

    added DELIM to compare_trials_AoA

--- Calimero/data analysis/Plotting UI/main_analysis.m
+++ Calimero/data analysis/Plotting UI/main_analysis.m
@@ -13,7 +13,7 @@ close all force
 
 % data_path = "F:\Calimero Data\Calimero 09_23_2025\"; % for comparing plot data across ladder, ascending, descending
 % data_path = "F:\Calimero Data\Calimero 09_23_2025_ascending\";
-data_path = uigetdir(".", "Select a 'plot data' folder") + "\";
+data_path = uigetdir(".", "Select a 'plot data' folder") + "/";
 if isequal(data_path, 0)
     disp('User canceled folder selection.');
 else
