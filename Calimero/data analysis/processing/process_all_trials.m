14077b0dd385705f914173b9836cf2f186f71780
ronangiss <46977169+ronangiss@users.noreply.github.com>
2026-01-31 15:33:26 -0500

    mac friendly file delimeter

--- Calimero/data analysis/processing/process_all_trials.m
+++ Calimero/data analysis/processing/process_all_trials.m
@@ -16,9 +16,11 @@ close all
 cd(fileparts(mfilename('fullpath')));
 addpath(genpath('../../'))
 
+DELIM = string(filesep);
+
 % Data stored in Dataset Name -> speed -> type + date
 
-slack_bool = true;
+slack_bool = false;
 slack_path = "R:\ENG_Breuer_Shared\group\Ronan\";
 % ADD PATH WHERE DATA SHOULD GET DUMPED
 
@@ -28,7 +30,7 @@ h = helpdlg("Please select the 'data' folder you'd like to process.");
 uiwait(h);     % ensure the user reads it before continuing
 
 % Open a file selection dialog and get the file path
-data_path = uigetdir(".", "Select the 'data' folder") + "\";
+data_path = uigetdir(".", "Select the 'data' folder") + DELIM;
 if isequal(data_path, 0)
     disp('User canceled folder selection.');
 else
@@ -48,19 +50,19 @@ for i = 3:length(contents)
 end
 
 if addFolders
-params_path = data_path + "/experiment parameters/";
+params_path = data_path + DELIM + "experiment parameters" + DELIM;
 
 [wind_speed, type, freq_vals, AoA_vals, amp, time_stamp] = eval_params(params_path);
 
 oldFolder = data_path;
-raw_appendage = "/raw data";
+raw_appendage = DELIM + "raw data";
 newFolder = data_path + raw_appendage;
 % Does 'raw data' folder already exist?
 if ~isfolder(newFolder)
     % make raw_data folder
     mkdir(newFolder)
     % move data into raw_data folder
-    movefile(oldFolder + "/*", newFolder)
+    movefile(oldFolder + DELIM + "*", newFolder)
     disp("raw data folder added")
 else
     disp("raw data folder found")
@@ -71,14 +73,14 @@ date = strjoin(parts(1:3), "_");
 
 % Put 'raw data' in type + date folder
 oldFolder = data_path;
-type_appendage = "/" + type + "_" + date;
+type_appendage = DELIM + type + "_" + date;
 newFolder = data_path + type_appendage;
 % Does type + date folder already exist?
 if ~isfolder(newFolder)
     % make type + date folder
     mkdir(newFolder)
     % move data into type + date folder
-    movefile(oldFolder + "/*", newFolder)
+    movefile(oldFolder + DELIM + "*", newFolder)
     disp("type_date folder added")
 else
     disp("type_date folder found")
@@ -86,14 +88,14 @@ end
 
 % Put type + date folder in speed folder
 oldFolder = data_path;
-speed_appendage = "/" + wind_speed + " m.s";
+speed_appendage = DELIM + wind_speed + " m.s";
 newFolder = data_path + speed_appendage;
 % Does 'raw data' folder already exist?
 if ~isfolder(newFolder)
     % make raw_data folder
     mkdir(newFolder)
     % move data into raw_data folder
-    movefile(oldFolder + "/*", newFolder)
+    movefile(oldFolder + DELIM + "*", newFolder)
     disp("speed folder added")
 else
     disp("speed folder found")
@@ -104,10 +106,10 @@ else
 
     filepath = extractBefore(data_path, "raw data");
     s = extractBefore(filepath, " m.s");
-    s_parts = split(s, "\");
+    s_parts = split(s, DELIM);
     wind_speed = str2num(s_parts(end));
 
-    s = erase(extractAfter(filepath, " m.s"), "\");
+    s = erase(extractAfter(filepath, " m.s"), DELIM);
     s_parts = split(s, "_");
     idx = -1;
     for j = 1:length(s_parts)
@@ -130,10 +132,10 @@ offsets_path = [];
 processed_data_path = [];
 wind_tunnel_path = [];
 
-raw_data_path = [raw_data_path filepath + "/raw data/experiment data/"];
-offsets_path = [offsets_path filepath + "/raw data/offsets data/"];
-processed_data_path = [processed_data_path filepath + "/processed data/"];
-wind_tunnel_path = [wind_tunnel_path filepath + "/raw data/wind tunnel data/"];
+raw_data_path = [raw_data_path filepath + DELIM + "raw data" + DELIM + "experiment data" + DELIM];
+offsets_path = [offsets_path filepath + DELIM + "raw data" + DELIM + "offsets data" + DELIM];
+processed_data_path = [processed_data_path filepath + DELIM + "processed data" + DELIM];
+wind_tunnel_path = [wind_tunnel_path filepath + DELIM + "raw data" + DELIM + "wind tunnel data" + DELIM];
 
 if isempty(raw_data_path)
     error("Oops, no data paths made")
@@ -146,19 +148,19 @@ for i = 1:length(filePattern)
     exp_files = [exp_files; dir(filePattern(i))];
 end
 
-dirPath = filepath + "/processed data";
+dirPath = filepath + DELIM + "processed data";
 if ~exist(dirPath, 'dir')
     mkdir(dirPath);
     fprintf('Directory "%s" created.\n', dirPath);
 end
 
 % Record log of outputs while processing data
-dirPath = filepath + "/processing logs";
+dirPath = filepath + DELIM + "processing logs";
 if ~exist(dirPath, 'dir')
     mkdir(dirPath);
     fprintf('Directory "%s" created.\n', dirPath);
 end
-diary(dirPath + "/" + wind_speed + "ms_" + type + ".txt")
+diary(dirPath + DELIM + wind_speed + "ms_" + type + ".txt")
 percent_complete = 0;
 try
 time_now = datetime;
